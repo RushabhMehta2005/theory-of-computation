7f74566d4b55376bb9726092c086974765eeb6f8
Rushabh Mehta <mehtarushabh2005@gmail.com>
2025-01-26 21:18:20 +0530

    Rename files

--- theory_of_computation/automata/__init__.py
+++ theory_of_computation/automata/__init__.py
@@ -1,4 +1,4 @@
 from .state import State, StateSet
 from .alphabet import Alphabet, AlphabetSet
 from .transition_function import TransitionFunction
-from .discrete_finite_automata import DFA
+from .deterministic_finite_automata import DFA
